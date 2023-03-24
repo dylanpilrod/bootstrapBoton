e9e3e63d5452def9f7c461306c05bcea0327114e
dylanpilrod <dylanprt.t53@gmail.com>
2023-03-24 19:14:33 +0100

    hecho

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+<h1>Esto es un boton</h1>
+<p>Puedes darle click si quieres, no va hacer nada. Como yo.</p>
+<button type="button" class="btn btn-primary">Hazme Click</button>
+<p>Dylan Pila Rodriguez</p>
+</div>
+    
+  );
+}
+
+export default App;
